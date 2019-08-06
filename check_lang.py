4719dbd1d8730ada883814f9c0b12efa9a4ce5f3
Luzian Bieri <l.j.bieri@gmail.com>
2019-08-06 11:16:58 +0200

    fixed edgecase of deleting a whole internal dict ant compare BOTH files against each other

--- check_lang.py
+++ check_lang.py
@@ -6,51 +6,52 @@ run as:
 python check_lang.py language_file1.json language_file2.json language_file3.json etc.
 """
 
+# TODO if a whole block is deleted check stuff
+
 
 def check_rec(dict1, dict2, pre_key=''):
     """[checks dict1 against dict2 recurisveley]
-    
+
     Arguments:
         dict1 {dict} -- [primary dict to compare]
         dict2 {dict} -- [dict to be compared against]
-    
+
     Keyword Arguments:
         pre_key {str} -- [key of before iteration to differentiate between different depths] (default: {''})
-    
+
     Returns:
         [dict] -- [dict of all missing key and value pairs]
     """
 
     # generate dict from all differences of the two dictionaries
-    res = {k: dict2[k] for k in set(dict2) - set(dict1)}
+    res = {k: dict1[k] for k in set(dict1) - set(dict2)}
 
     for key in dict1.keys():
-        #ignore all simple values or keys which have been caught by the previous iteration
-        if key not in res and isinstance(dict1[key], dict):
+        # ignore all simple values or keys which have been caught by the previous iteration
+        if key not in res.keys() and isinstance(dict1[key], dict):
             # append all new inconsitencies
             inner_res = check_rec(dict1[key], dict2[key], key)
             if inner_res != {}:
-                res.update({key: inner_res}) 
+                res.update({key: inner_res})
 
     return res
 
 
 def ckeck_dicts(dicts):
     """compares all given dicts against each other
-    
+
     Arguments:
         dicts {dict} -- [dict of dicts to compare, the key is the dict-name]
-    
+
     Returns:
         [bool] -- [success: True if all keys are filled in all dicts]
     """
-
 
     success = True
     for key, dict in dicts.items():
         print(f'-going through {key}')
         rest = dicts.copy()
-        del rest[key] # prohibit self checking a file
+        del rest[key]  # prohibit self checking a file
 
         for comp_key, compare in rest.items():
             print(f'  - comparing against {comp_key}')
@@ -61,18 +62,17 @@ def ckeck_dicts(dicts):
             if result != {}:
                 success = False
                 print(
-                    f'    ---> Missing {result} in file {key} (compared against file {comp_key})')
+                    f'    ---> Missing {result} in file {comp_key} (compared against file {key})')
 
     return success
 
 
 def main(argv):
-
     """reads in all languagefiles given as arguments, assembles them to compare.
     """
 
     if len(argv) < 3:
-        print('check_lang.py <file_1> <file_2> <file_3> ...')
+        print('Usage: \n\tcheck_lang.py <file_1> <file_2> <file_3> ...')
         sys.exit()
 
     lang = {}
